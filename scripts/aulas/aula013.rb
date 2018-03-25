0a8953d36f0a1954d48b4d2199cd2da5929baa29
mrrafagomides <mrrafagomides@gmail.com>
2018-03-25 00:30:51 -0400

    Updates

---
+++ scripts/aulas/aula013.rb
@@ -0,0 +1,11 @@
+# Arrays
+
+a = [1,2,3,4,5,6,7,8,9,0]
+puts a.class
+puts a.inspect
+puts a
+puts a[2]
+
+v = Array.new
+v = push(4)
+puts v.inspect
---
+++ scripts/aulas/aula013c.rb
@@ -0,0 +1,9 @@
+h = {'a' => 123, 'b' => 899}
+puts h.class
+puts h
+puts h["a"]
+puts h['b']
+
+h.merge!({'Teste' => 'Rafa'})
+
+puts h
