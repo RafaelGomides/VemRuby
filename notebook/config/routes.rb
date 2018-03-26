17b087757143dbb5bbfe2ad18324b378226c80e1
mrrafagomides <mrrafagomides@gmail.com>
2018-03-26 13:32:53 -0400

    Projeto Notebook

--- notebook/config/routes.rb
+++ notebook/config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :phones
+  resources :addresses
+  resources :contacts
+  resources :contacts
+  resources :contacts
+  resources :kinds
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
---
+++ notebook/db/migrate/20180326165956_create_addresses.rb
@@ -0,0 +1,12 @@
+class CreateAddresses < ActiveRecord::Migration
+  def change
+    create_table :addresses do |t|
+      t.string :street
+      t.string :city
+      t.string :state
+      t.reference :contact
+
+      t.timestamps null: false
+    end
+  end
+end
